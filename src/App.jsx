a13fbc114dccb44f6dedf269768201aaae6e7b15
Kurakula Balaji <kurakulabalaji2005@gmail.com>
2025-06-24 17:08:53 +0530

    Movie streaming application commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>This is a movie streaming application</div>
+  )
+}
+
+export default App
